40c81ce24c777d0667e82ef9b08a8d4f3faa3ebd
Mahmoud Mohamed Ahmed <mahmoudsaad6675@gmail.com>
2023-02-02 20:03:36 +0200

    changing show page style

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -37,7 +37,7 @@
    
                     <a class="btn btn-primary" style="width: 34px; height:25px; padding:0px;" href="{{route('posts.show', $post['id'])}}">
                         <svg style="width: 100%; height:100%"
-                             xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                             xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                 <path d="M12 12m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
                                 <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
@@ -45,7 +45,7 @@
     
                     <a class="btn btn-success" style="width: 34px; height:25px; padding:0px;" href="{{ route('posts.edit', $post['id']) }}">
                         <svg style="width: 100%; height:90%"
-                         xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                         xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z"></path>
                             <path d="M16 5l3 3"></path>
@@ -57,7 +57,7 @@
                     <a class="btn btn-danger" style="width: 34px; height:25px; padding:0px;" href="{{ route('posts.edit', $post['id']) }}">
                     <button type="submit" style="display:none;" onclick="return confirm('Are you sure?')">
                         
-                         </button><svg style="width: 100%; height:90%" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                         </button><svg style="width: 100%; height:90%" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M4 7l16 0"></path>
                             <path d="M10 11l0 6"></path>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,43 +4,25 @@
 
 <div class="card text-center">
     <div class="card-header">
-        Add post
+        POST
     </div>
-    <div class="card-body">
-        <form style="border-radius:20px; border:2px solid #007bff; margin: 10px; ">
-            <div class="form-group" style=" margin: 20px; ">
-                Title :<input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    readonly="readonly"
-                    value="{{$post['title']}}">
+    
+    <div class="card-body" style="border-radius:20px; margin: 10px; background-color:;">
+        <div class="card  bg-primary mb-3" style="border-radius:10px; border: #007bff;">
+            <div class="card-header text-white">Post #{{$post['id']}}</div>
+            <div class="card-body" style="background-color:white; border:2px solid #007bff;">
+              <h5 class="card-title"><b>- {{$post['title']}} - </b></h5>
+              <p class="card-text">{{$post['description']}}</p>
+              <hr> Posted by : {{$post->user?->name}}<br>
+              At : {{$post['created_at']}}
             </div>
-            <div class="form-group " style=" margin: 20px; ">
-                Description :<textarea
-                    class="form-control"
-                    id="exampleFormControlTextarea1"
-                    rows="3"
-                    readonly="readonly">{{$post['description']}}</textarea>
-            </div>
-            <div class="form-group" style=" margin: 20px; ">
-                Poster creator :<input
-                    type="email"
-                    class="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    readonly="readonly"
-                    value="{{$post->user?->name}}">
-            </div>
-        </form>
+          </div>
         
-        <a href="{{route('posts.index')}}" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="20" height="20" viewBox="0 0 25 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-            <path d="M5 12l14 0"></path>
-            <path d="M5 12l6 6"></path>
-            <path d="M5 12l6 -6"></path>
-         </svg>Back</a>
+        
+        <a href="{{route('posts.index')}}" class="btn btn-primary">
+            <svg
+             xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-left" width="20" height="20" viewBox="5 1 15 24" stroke-width="2" stroke="currentColor" fill="none"><path d="M5 12l14 0"></path><path d="M5 12l6 6"></path><path d="M5 12l6 -6"></path></svg>
+         Back</a>
     </div>
 </div>
 
